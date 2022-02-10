a7663a40660908d0f803401b6cb6a62e7dc1e6c7
luke choi <primera_jh@hotmail.com>
2022-02-10 20:04:08 +0900

    firestore unknown error

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/resources/storage_method.dart';
 
 class AuthMethod {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -17,25 +18,33 @@ class AuthMethod {
     required Uint8List file,
   }) async {
     String res = "some error occurred";
-    try {
 
-      if(email.isNotEmpty ||password.isNotEmpty ||username.isNotEmpty ||bio.isNotEmpty || file != null){
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty) {
         //register user
-        UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
         print(cred.user!.uid);
+
+       String photoUrl = await StorageMethods()
+            .uploadImageToStorage('profilePics', file);
         // add user to our database
         await _firestore.collection('users').doc(cred.user!.uid).set({
-          'username':username,
-          'uid':cred.user!.uid,
-          'email':email,
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
           'bio': bio,
-          'followers':[],
-          'following':[]
+          'followers': [],
+          'following': [],
+          'photoUrl': photoUrl,
         });
         res = "success";
       }
-    }catch(e){
-      res=e.toString();
+    } catch (e) {
+      res = e.toString();
     }
     return res;
   }
--- lib/resources/storage_method.dart
+++ lib/resources/storage_method.dart
@@ -4,12 +4,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-class Storage {
+class StorageMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //add image to firebase store
-  Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost) async{
+  Future<String> uploadImageToStorage(String childName, Uint8List file) async {
 
     Reference ref = _storage.ref().child(childName).child(_auth.currentUser!.uid);
     UploadTask uploadTask= ref.putData(file);
