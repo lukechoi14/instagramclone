9c8ffd7167276f87759febef476e5eb9088461b6
luke choi <primera_jh@hotmail.com>
2022-02-13 18:09:02 +0900

    user class& constructor

---
+++ lib/models/user.dart
@@ -0,0 +1,19 @@
+class User {
+  final String email;
+  final String uid;
+  final String photoUrl;
+  final String username;
+  final String bio;
+  final List followers;
+  final List following;
+
+  const User({
+    required this.email,
+    required this.uid,
+    required this.photoUrl,
+    required this.username,
+    required this.bio,
+    required this.followers,
+    required this.following,
+  });
+}
