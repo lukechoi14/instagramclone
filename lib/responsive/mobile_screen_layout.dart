215a6653e0c05090f506b20ef9b33b203479ce50
luke choi <primera_jh@hotmail.com>
2022-02-14 15:33:01 +0900

    add_post_screen.dart

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variables.dart';
 import 'package:provider/provider.dart';
 import 'package:instagram_clone/models/user.dart' as model;
 
@@ -13,7 +14,8 @@ class MobileScreenLayout extends StatefulWidget {
   @override
   State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
 }
-class _MobileScreenLayoutState extends State<MobileScreenLayout>{
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   // String username = "";
   //
   // void initState(){
@@ -27,16 +29,63 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout>{
   //     username = (snap.data() as Map<String, dynamic>)['username'];
   //   });
   // }
+  late PageController pageController;
+  int _page = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = PageController();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    pageController.dispose();
+  }
+  void navigationTapped(int page){
+    pageController.jumpToPage(page);
+  }
+  void onPageChanged(int page) {
+    setState(() {
+      _page =page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(child: Text("this is mobile screen")),
-      // bottomNavigationBar: CupertinoTabBar(
-      //   items: [
-      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: '', backgroundColor: primaryColor)
-      //   ],
-      // ),
+      body: PageView(
+        children: HomeScreenItems,
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        onTap: navigationTapped,
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home,color: _page==0?primaryColor:secondaryColor,), label: '', backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search,color: _page==1?primaryColor:secondaryColor,),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle,color: _page==2?primaryColor:secondaryColor,),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite,color: _page==3?primaryColor:secondaryColor,),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person,color: _page==4?primaryColor:secondaryColor,),
+              label: '',
+              backgroundColor: primaryColor)
+        ],
+      ),
     );
   }
 }
