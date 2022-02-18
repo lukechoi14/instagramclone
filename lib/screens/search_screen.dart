a2ee67e8cc151a51d95ecb4b75ba4990ef691a89
luke choi <primera_jh@hotmail.com>
2022-02-18 17:39:38 +0900

    showing post on search screen with grid view

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -23,43 +24,77 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: TextFormField(
-          controller: searchController,
-          decoration: InputDecoration(labelText: 'Search for a user'),
-          onFieldSubmitted: (String _) {
-            setState(() {
-              isShowUsers = true;
-            });
-          },
-        ),
-      ),
-      body: isShowUsers? FutureBuilder(
-        future: FirebaseFirestore.instance
-            .collection('users')
-            .where('username', isGreaterThanOrEqualTo: searchController.text)
-            .get(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return ListView.builder(
-              itemCount: (snapshot.data! as dynamic).docs.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        (snapshot.data! as dynamic).docs[index]['photoUrl']),
-                  ),
-                  title:
-                      Text((snapshot.data! as dynamic).docs[index]['username']),
-                );
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: TextFormField(
+            controller: searchController,
+            decoration: InputDecoration(labelText: 'Search for a user'),
+            onFieldSubmitted: (String _) {
+              setState(() {
+                isShowUsers = true;
               });
-        },
-      ): Text('Posts')
-    );
+            },
+          ),
+        ),
+        body: isShowUsers
+            ? FutureBuilder(
+          future: FirebaseFirestore.instance
+              .collection('users')
+              .where('username',
+              isGreaterThanOrEqualTo: searchController.text)
+              .get(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+                itemCount: (snapshot.data! as dynamic).docs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          (snapshot.data! as dynamic).docs[index]
+                          ['photoUrl']),
+                    ),
+                    title: Text((snapshot.data! as dynamic).docs[index]
+                    ['username']),
+                  );
+                });
+          },
+        )
+            : FutureBuilder(
+            future: FirebaseFirestore.instance.collection('posts').get(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return GridView.builder(
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1,
+                    //1 개의 행에 보여줄 item 개수
+                    // childAspectRatio: MediaQuery.of(context).size.width /
+                    //     (MediaQuery.of(context).size.height / 1.2), //item 의 가로 1, 세로 1 의 비율
+                    mainAxisSpacing: 8, //수평 Padding
+                    crossAxisSpacing: 8, //수직 Padding
+                  ),
+                  itemBuilder: (context, index) => GridTile(
+                    child: Image.network(
+                        (snapshot.data! as dynamic).docs[index]['postUrl'],fit: BoxFit.cover,),
+                  ),
+              );
+              // StaggeredGridView.countBuilder(
+              //   crossAxisCount: 3,
+              //   mainAxisSpacing: 8,
+              //   crossAxisSpacing: 8,
+              //   itemCount: (snapshot.data! as dynamic).docs.length,
+              //   itemBuilder: (context, index) => Image.network(
+              //       (snapshot.data! as dynamic).docs[index]['postUrl']),
+              //   staggeredTileBuilder: (index) => StaggeredTile.count(
+              //       (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1));
+            }));
   }
 }
