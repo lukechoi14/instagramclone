5d35d9eaf796e64f05e41c04241d943e78a77d13
luke choi <primera_jh@hotmail.com>
2022-02-21 13:03:25 +0900

    getting posts from firestore in profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -8,6 +8,7 @@ import 'package:instagram_clone/widgets/follow_button.dart';
 
 class ProfileScreen extends StatefulWidget {
   final String uid;
+
   const ProfileScreen({Key? key, required this.uid}) : super(key: key);
 
   @override
@@ -15,98 +16,174 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  var userData= {};
+  var userData = {};
   int postLen = 0;
   int followers = 0;
   int following = 0;
+  bool isFollowing = false;
+  bool isLoading = false;
+
   @override
   void initState() {
     super.initState();
     getData();
   }
+
   getData() async {
-    try{
-      var userSnap = await FirebaseFirestore.instance.collection('users').doc(widget.uid).get();
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      var userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.uid)
+          .get();
       //get post length
-      var postSnap = await FirebaseFirestore.instance.collection('posts').where('uid',isEqualTo: FirebaseAuth.instance.currentUser!.uid).get();
+      var postSnap = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get();
       postLen = postSnap.docs.length;
       userData = userSnap.data()!;
       followers = userSnap.data()!['followers'].length;
       following = userSnap.data()!['following'].length;
-      setState(() {
-
-      });
-    }
-    catch(e){
+      isFollowing = userSnap
+          .data()!['followers']
+          .contains(FirebaseAuth.instance.currentUser!.uid);
+      setState(() {});
+    } catch (e) {
       showSnackBar(e.toString(), context);
     }
+    setState(() {
+      isLoading = false;
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: Text(userData['username']),
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
+    return isLoading
+        ? Center(
+            child: const CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              title: Text(userData['username']),
+            ),
+            body: ListView(
               children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      backgroundImage: NetworkImage(
-                         userData['photoUrl']),
-                      radius: 40,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            buildStatColumn(postLen, 'Post'),
-                            buildStatColumn(followers, 'Follower'),
-                            buildStatColumn(following, 'Following'),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            FollowButton(
-                              backgroundColor: mobileBackgroundColor,
-                              borderColor: Colors.grey,
-                              text: 'Edit Profile',
-                              textColor: primaryColor,
-                              function: () {},
-                            )
-                          ],
-                        )
-                      ]),
-                    ),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            backgroundImage: NetworkImage(userData['photoUrl']),
+                            radius: 40,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Column(children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  buildStatColumn(postLen, 'Post'),
+                                  buildStatColumn(followers, 'Follower'),
+                                  buildStatColumn(following, 'Following'),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  FirebaseAuth.instance.currentUser!.uid ==
+                                          widget.uid
+                                      ? FollowButton(
+                                          backgroundColor:
+                                              mobileBackgroundColor,
+                                          borderColor: Colors.grey,
+                                          text: 'Edit Profile',
+                                          textColor: primaryColor,
+                                          function: () {},
+                                        )
+                                      : isFollowing
+                                          ? FollowButton(
+                                              backgroundColor: Colors.white,
+                                              borderColor: Colors.grey,
+                                              text: 'Unfollow',
+                                              textColor: Colors.black,
+                                              function: () {},
+                                            )
+                                          : FollowButton(
+                                              backgroundColor: Colors.blue,
+                                              borderColor: Colors.blue,
+                                              text: 'Follow',
+                                              textColor: Colors.white,
+                                              function: () {},
+                                            )
+                                ],
+                              )
+                            ]),
+                          ),
+                        ],
+                      ),
+                      Container(
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Text(
+                            userData['username'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )),
+                      Container(
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.only(top: 1),
+                          child: Text(
+                            userData['bio'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ))
+                    ],
+                  ),
                 ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(top:15),
-                  child: Text(userData['username'], style: TextStyle(fontWeight: FontWeight.bold),)
-                ),
-                Container(
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.only(top:1),
-                    child: Text(userData['bio'], style: TextStyle(fontWeight: FontWeight.bold),)
-                )
+                Divider(),
+                FutureBuilder(
+                    future: FirebaseFirestore.instance
+                        .collection('posts')
+                        .where('uid', isEqualTo: widget.uid)
+                        .get(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return GridView.builder(
+                          shrinkWrap: true,
+                          itemCount: (snapshot.data! as dynamic).docs.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3,
+                                  crossAxisSpacing: 5,
+                                  mainAxisSpacing: 1.5,
+                                  childAspectRatio: 1),
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot snap =
+                                (snapshot.data! as dynamic).docs[index];
+                            return Container(
+                              child: Image(
+                                image: NetworkImage(
+                                    snap['postUrl']),
+                                fit: BoxFit.cover,
+                              ),
+                            );
+                          });
+                    })
               ],
             ),
-          ),
-          Divider()
-        ],
-      ),
-    );
+          );
   }
 }
 
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -38,61 +39,72 @@ class _SearchScreenState extends State<SearchScreen> {
         ),
         body: isShowUsers
             ? FutureBuilder(
-          future: FirebaseFirestore.instance
-              .collection('users')
-              .where('username',
-              isGreaterThanOrEqualTo: searchController.text)
-              .get(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return ListView.builder(
-                itemCount: (snapshot.data! as dynamic).docs.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          (snapshot.data! as dynamic).docs[index]
-                          ['photoUrl']),
-                    ),
-                    title: Text((snapshot.data! as dynamic).docs[index]
-                    ['username']),
-                  );
-                });
-          },
-        )
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .where('username',
+                        isGreaterThanOrEqualTo: searchController.text)
+                    .get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView.builder(
+                      itemCount: (snapshot.data! as dynamic).docs.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => ProfileScreen(
+                                      uid: (snapshot.data! as dynamic)
+                                          .docs[index]['uid']))),
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  (snapshot.data! as dynamic).docs[index]
+                                      ['photoUrl']),
+                            ),
+                            title: Text((snapshot.data! as dynamic).docs[index]
+                                ['username']),
+                          ),
+                        );
+                      });
+                },
+              )
             : FutureBuilder(
-            future: FirebaseFirestore.instance.collection('posts').get(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              return GridView.builder(
-                  itemCount: (snapshot.data! as dynamic).docs.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 1,
-                    //1 개의 행에 보여줄 item 개수
-                    // childAspectRatio: MediaQuery.of(context).size.width /
-                    //     (MediaQuery.of(context).size.height / 1.2), //item 의 가로 1, 세로 1 의 비율
-                    mainAxisSpacing: 8, //수평 Padding
-                    crossAxisSpacing: 8, //수직 Padding
-                  ),
-                  itemBuilder: (context, index) => Image.network(
-                      (snapshot.data! as dynamic).docs[index]['postUrl'],fit: BoxFit.cover,),
-              );
-              // StaggeredGridView.countBuilder(
-              //   crossAxisCount: 3,
-              //   mainAxisSpacing: 8,
-              //   crossAxisSpacing: 8,
-              //   itemCount: (snapshot.data! as dynamic).docs.length,
-              //   itemBuilder: (context, index) => Image.network(
-              //       (snapshot.data! as dynamic).docs[index]['postUrl']),
-              //   staggeredTileBuilder: (index) => StaggeredTile.count(
-              //       (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1));
-            }));
+                future: FirebaseFirestore.instance.collection('posts').get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  return GridView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      childAspectRatio: 1,
+                      //1 개의 행에 보여줄 item 개수
+                      // childAspectRatio: MediaQuery.of(context).size.width /
+                      //     (MediaQuery.of(context).size.height / 1.2), //item 의 가로 1, 세로 1 의 비율
+                      mainAxisSpacing: 8,
+                      //수평 Padding
+                      crossAxisSpacing: 8, //수직 Padding
+                    ),
+                    itemBuilder: (context, index) => Image.network(
+                      (snapshot.data! as dynamic).docs[index]['postUrl'],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                  // StaggeredGridView.countBuilder(
+                  //   crossAxisCount: 3,
+                  //   mainAxisSpacing: 8,
+                  //   crossAxisSpacing: 8,
+                  //   itemCount: (snapshot.data! as dynamic).docs.length,
+                  //   itemBuilder: (context, index) => Image.network(
+                  //       (snapshot.data! as dynamic).docs[index]['postUrl']),
+                  //   staggeredTileBuilder: (index) => StaggeredTile.count(
+                  //       (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1));
+                }));
   }
 }
